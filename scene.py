cb2b098dee78ad651ec4a364063f52e02f66bab8
samuel22212 <samuelporter219@gmail.com>
2023-01-30 18:26:46 -0700

    ...

--- scene.py
+++ scene.py
@@ -19,6 +19,10 @@ def main():
     draw_sky(canvas, scene_width, scene_height)
     draw_ground(canvas,scene_width,scene_height)
     draw_clouds(canvas, 400,400,350,370)
+    draw_road(canvas, scene_width,scene_height)
+    draw_road_lines(canvas,415,400)
+    
+    draw_clouds(canvas, scene_width - 100, scene_height - 200, scene_width - 150, scene_height - 225)
     # Call the finish_drawing function
     # in the draw2d.py library.
     finish_drawing(canvas)
@@ -32,10 +36,22 @@ def draw_sky(canvas, scene_width, scene_height):
         scene_width, scene_height, width=0, fill="sky blue")
 
 def draw_clouds(canvas,cloud_x1,cloud_y1,cloud_x2,cloud_y2):
-    draw_oval(canvas, cloud_x1,cloud_y1,cloud_x2,cloud_y2, fill= "white")
+    draw_oval(canvas, cloud_x1,cloud_y1,cloud_x2,cloud_y2, fill= "white", width= 0)
+    draw_oval(canvas, cloud_x1+15,cloud_y1,cloud_x2+30,cloud_y2, fill="white", width=0)    
 
 def draw_ground(canvas,scene_width,scene_height):
     draw_rectangle(canvas,0, 0, scene_width, scene_height/3, width=0,  fill ="green")
+
+def draw_road(canvas, scene_width, scene_height):
+    draw_rectangle(canvas,300, 0, scene_width/1.5, scene_height/3, width=0,  fill ="black")
+
+#road lines different sizes on purpose
+def draw_road_lines(canvas,line_width_min,line_width_max):
+    draw_rectangle(canvas, line_width_min,20,line_width_max,50, fill = "yellow", width=1) 
+    draw_rectangle(canvas, line_width_min,60,line_width_max,80, fill = "yellow", width=1) 
+    draw_rectangle(canvas, line_width_min,90,line_width_max,120, fill = "yellow", width=1) 
+    draw_rectangle(canvas, line_width_min,130,line_width_max,150, fill = "yellow", width=1) 
+
 
 # Call the main function so that
 # this program will start executing.
