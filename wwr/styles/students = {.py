2cca02e6db68470793620bb95c0facbd14f59e99
samuel22212 <samuelporter219@gmail.com>
2023-05-13 20:02:04 -0600

    trying to get github fixed

---
+++ wwr/styles/students = {.py
@@ -0,0 +1,9 @@
+students = {
+    "10-450-1203": "Jorge Soares",
+    "75-421-2310": "Abdul Ali",
+    "07-103-5621": "Michelle Davis"
+}
+
+students["81-298-9238"] = "Sama Patel"
+name = students["75-421-2310"]
+print(name)
